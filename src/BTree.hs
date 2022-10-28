79aeae41d8ad8141155026229ba005f197623525
Junichi Kato <j5ik2o@gmail.com>
2022-10-28 09:28:01 +0900

    first commit

---
+++ src/BTree.hs
@@ -0,0 +1,48 @@
+module BTree where
+
+import Prelude hiding (sum, max, min)
+
+data Node a = Branch a (Node a) (Node a) | Leaf a
+
+instance (Show a) => Show (Node a) where
+  show (Leaf n) = show n
+  show (Branch n l r) = "(" ++ show l ++ " " ++ show n ++ " " ++ show r ++ ")"
+
+instance (Eq a) => Eq (Node a) where
+  (==) (Leaf a) (Leaf b) = a == b
+  (==) (Branch a l r) (Branch b l' r') = a == b && l == l' && r == r'
+  (==) _ _ = False
+
+instance Functor Node where
+  fmap f (Leaf n) = Leaf (f n)
+  fmap f (Branch n l r) = Branch (f n) (fmap f l) (fmap f r)
+
+value :: Node a -> a
+value (Leaf x) = x
+value (Branch x _ _) = x
+
+size :: Node a -> Int
+size (Leaf _) = 1
+size (Branch _ l r) = 1 + size l + size r
+
+sum :: (Num a) => Node a -> a
+sum (Leaf x) = x
+sum (Branch x l r) = x + sum l + sum r
+
+avg :: (Fractional a) => Node a -> a
+avg n = sum n / fromIntegral (size n)
+
+max :: (Num a) => Node a -> a
+max (Leaf x) = x
+max (Branch _ _ r) = max r
+
+min :: (Num a) => Node a -> a
+min (Leaf x) = x
+min (Branch _ l _) = min l
+
+find :: (Ord a) => Node a -> a -> Maybe (Node a)
+find n@(Leaf nv) v = if nv == v then Just n else Nothing
+find n@(Branch nv l r) v
+  | nv == v = Just n
+  | v < nv = find l v
+  | nv < v = find r v
---
+++ test/BTreeSpec.hs
@@ -0,0 +1,42 @@
+module BTreeSpec where
+
+import Prelude hiding (sum, max, min)
+import Test.Hspec
+import BTree
+
+spec :: Spec
+spec =
+  describe "BTree" $ do
+    it "size Leaf" $ do
+      size (Leaf 1) `shouldBe` 1
+    it "size Branch" $ do
+      size (Branch 1 (Leaf 1) (Leaf 2)) `shouldBe` 3
+    it "sum Leaf" $ do
+      sum (Leaf 1) `shouldBe` 1
+    it "size Branch" $ do
+      sum (Branch 1 (Leaf 1) (Leaf 2)) `shouldBe` 4
+    it "avg Leaf" $ do
+      avg (Leaf 1) `shouldBe` 1
+    it "avg Branch" $ do
+      avg (Branch 1 (Leaf 1) (Leaf 1)) `shouldBe` 1.0
+    it "min Leaf" $ do
+      min (Leaf 1) `shouldBe` 1
+    it "min Branch" $ do
+      min (Branch 2 (Leaf 1) (Leaf 3)) `shouldBe` 1
+    it "max Leaf" $ do
+      max (Leaf 1) `shouldBe` 1
+    it "max Branch" $ do
+      max (Branch 2 (Leaf 1) (Leaf 3)) `shouldBe` 3
+    it "find Leaf" $ do
+      let n = Leaf 1
+      find n 1 `shouldBe` Just n
+    it "find Branch" $ do
+      let n1 = Leaf 3
+      let n2 = Branch 2 (Leaf 1) n1
+      find n2 3 `shouldBe` Just n1
+    it "fmap Leaf" $ do
+      let n = Leaf 1
+      fmap (*2) n `shouldBe` Leaf 2
+    it "fmap Branch" $ do
+      let n2 = Branch 2 (Leaf 1) (Leaf 3)
+      fmap (*2) n2 `shouldBe` Branch 4 (Leaf 2) (Leaf 6)
